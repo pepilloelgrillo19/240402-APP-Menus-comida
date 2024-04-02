5af445ecdb43e0e6ffbf5ae338ad614ea66aae44
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-04-02 15:32:50 +0200

    Initial Commit

---
+++ 240402 APP Menus comida/_40402_APP_Menus_comidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40402_APP_Menus_comidaApp.swift
+//  240402 APP Menus comida
+//
+//  Created by Luis on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _40402_APP_Menus_comidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 240402 APP Menus comidaUITests/_40402_APP_Menus_comidaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _40402_APP_Menus_comidaUITestsLaunchTests.swift
+//  240402 APP Menus comidaUITests
+//
+//  Created by Luis on 2/4/24.
+//
+
+import XCTest
+
+final class _40402_APP_Menus_comidaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
